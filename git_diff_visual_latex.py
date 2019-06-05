5e4c2767108acb2298c56c9d28e79f429a23d0fb
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-06-04 20:16:17 -0400

    Polish latex git visual diffing

--- git_diff_visual_latex.py
+++ git_diff_visual_latex.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""Utility for visually diffing latex projects.
+
+Example calls:
+git_diff_visual_latex git:HEAD dir:.
+git_diff_visual_latex git:HEAD^1 git:HEAD
+git_diff_visual_latex git:5f4a4b git:~/Desktop/paper
+"""
 
 import subprocess
 from subprocess import run
@@ -15,34 +22,40 @@ def do_checkout(output_dir, commit_ref):
   return run(make_checkout_args(output_dir, commit_ref), check=True)
 
 
-def do_latex(latex_dir, output_pdf_path):
+def make_pdf_dir(latex_dir, output_pdf_path):
     with tempfile.TemporaryDirectory() as output_dir:
       run(["latexmk", "-pdf", f"-output-directory={output_dir}", "main.tex"], cwd=latex_dir, check=True)
       # TODO check latex error
       shutil.copy2(os.path.join(output_dir, "main.pdf"), output_pdf_path)
+      return None
 
 
-def make_pdf(commit_ref, output_pdf_path):
+def make_pdf_git(commit_ref, output_pdf_path):
   with tempfile.TemporaryDirectory() as output_dir:
     do_checkout(output_dir, commit_ref)
-    do_latex(output_dir, output_pdf_path)
+    return make_pdf_dir(output_dir, output_pdf_path)
 
 
-def make_pdf_worktree(output_pdf_path):
-    latex_dir = os.getcwd()
-    do_latex(latex_dir, output_pdf_path)
+def make_pdf_dispatch(latex_project_spec, output_pdf_path):
+  (kind, spec) = latex_project_spec.split(":", 1)
+  method_name = f"make_pdf_{kind}"
+  method = globals()[method_name]
+  return method(spec, output_pdf_path)
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("commits", nargs="*")
+  print(__doc__)
+  parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument("ref_from", help="ref from")
+  parser.add_argument("ref_to", help="ref to")
   args = parser.parse_args()
 
   diff_dir = "pdf_diff"
-  print(diff_dir)
-  #make_pdf(args.commits[0], os.path.join(diff_dir, "main__a.pdf"))
-  #make_pdf(args.commits[1], os.path.join(diff_dir, "main__b.pdf"))
+  for i in [0,1]:
+    spec = args.references[i]
+    print(f"make {spec}")
+    make_pdf_dispatch(args.references[i], os.path.join(diff_dir, f"main__{i}.pdf"))
   print(f"done making pdfs at {diff_dir}")
 
   diffpdf_sh = os.path.normpath(os.path.join(os.path.realpath(__file__), "../diffpdf.sh"))
-  run([diffpdf_sh, "main__a.pdf", "main__b.pdf"], cwd=diff_dir, check=False)
+  run([diffpdf_sh, "main__0.pdf", "main__1.pdf"], cwd=diff_dir, check=False)
